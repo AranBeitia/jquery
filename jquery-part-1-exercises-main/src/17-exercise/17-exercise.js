766f7e2b4d0f7b89553e9ce34f1ff05f7f900c36
AranBeitia <arantza.beitia@gmail.com>
2021-07-29 15:15:09 +0200

    finish exercises part 1

--- jquery-part-1-exercises-main/src/17-exercise/17-exercise.js
+++ jquery-part-1-exercises-main/src/17-exercise/17-exercise.js
@@ -2,6 +2,10 @@ import $ from "jquery";
 
 function exercise17() {
   // Complete the code of the function
+  let list = $('.ex-list').children()
+
+  list.each( (i, item )=> $(item).css('font-weight', 'bold'))
+  console.log(list);
 }
 exercise17()
 export default exercise17;
